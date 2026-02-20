8fab57c215d0175913f4ca1c1eaff6c36360adef
Sam Skiller <samfundi9@gmail.com>
2026-02-21 02:35:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -7,6 +9,8 @@ import jwt from "jsonwebtoken";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 /* =========================
@@ -293,6 +297,11 @@ seedDatabase();
 /* =========================
    START SERVER
 ========================= */
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Sanctuary Server running on port ${PORT}`);
